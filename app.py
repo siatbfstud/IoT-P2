3e18ff48b59bb3ccb998f78e1d63a171c3c5d0c9
siatbfstud <tais0340@stud.kea.dk>
2022-03-15 11:49:50 +0100

    flask push

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+import datetime
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# New functions
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run()
