6d1fc700cb07a819146a04e532b1751c76048ea2
siatbfstud <tais0340@stud.kea.dk>
2022-03-17 10:37:52 +0100

    cronjob sucks

--- logDHT.py
+++ logDHT.py
@@ -3,29 +3,34 @@ import board
 import adafruit_dht
 import sqlite3
 
-# init db and frequency
 dbname="sensorsData.db"
-sampleFreq = 5 # time in seconds ==> Sample each 1 min
+sampleFreq = 1
 dhtDevice = adafruit_dht.DHT11(board.D16)
 
-# get dht data or update it
-def getDHTdata():	
+def getDHTdata():
     temperature = dhtDevice.temperature
     humidity = dhtDevice.humidity
     return temperature, humidity
 
-# log sensor data on database
 def logData(temperature, humidity):
-	conn=sqlite3.connect(dbname)
-	curs=conn.cursor()
-	curs.execute("INSERT INTO DHT_data values(datetime('now'), (?), (?))", (temperature, humidity))
-	conn.commit()
-	conn.close()
+    conn=sqlite3.connect(dbname)
+    curs=conn.cursor()
+    curs.execute("INSERT INTO DHT_data values(datetime('now'), (?), (?))", (temperature, humidity))
+    conn.commit()
+    conn.close()
 
 while True:
-    temperature, humidity = getDHTdata()
-    logData(temperature, humidity)
-    print("did it")
+    try:
+        temperature, humidity = getDHTdata()
+        logData(temperature, humidity)
+    
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+    
     time.sleep(sampleFreq)
-
-
