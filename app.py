06fd962bf276b4dfb028ee4d6359e98a42b6a241
oliverthers <oliv637p@stud.kea.dk>
2022-03-25 14:29:29 +0100

    vi done

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask,render_template,url_for,request,redirect, make_response
 import json
-
-from time import time
+import RPi.GPIO as GPIO
 
 from flask import Flask, render_template, make_response
 app = Flask(__name__)
@@ -10,32 +9,84 @@ import sqlite3
 conn=sqlite3.connect('tempDB.db', check_same_thread=False)
 curs=conn.cursor()
 
+class var:
+    mode = 0
+    Temperature = None
+
+PWMcool = 18
+PWMpel = 12
+PWMvarme = 13
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PWMcool,GPIO.OUT)
+GPIO.setup(PWMpel,GPIO.OUT)
+GPIO.setup(PWMvarme,GPIO.OUT)
+GPIO.setwarnings(False)
+cooler = GPIO.PWM(PWMcool,1000)		#create PWM instance with frequency
+peltier = GPIO.PWM(PWMpel, 1000)    #create PWM instance with frequency
+varme = GPIO.PWM(PWMvarme, 1000)    #create PWM instance with frequency
+
+
 # Retrieve LAST data from database
 def getLastData():
     for row in curs.execute("SELECT * FROM tempData ORDER BY timestamp DESC LIMIT 1"):
         dataTime = str(row[0])
         dataTemp = row[1]
-    #conn.close()
     return dataTime, dataTemp
 
 @app.route('/', methods=["GET", "POST"])
 def main():
     return render_template('index.html')
 
-@app.route('/option1') # 0-2 grader (fisk)
+@app.route('/option1') # 0-2 grader (fisk)/Test
 def option1():
-    print ("Setting prefered temperature to 0-2 degrees")
-    return ("nothing")
+    var.mode = 1
+    while var.mode == 1:
+        if var.Temperature > 2:
+            cooler.start(0)
+            peltier.start(0)
+            cooler.ChangeDutyCycle(100)
+            peltier.ChangeDutyCycle(70)
+        elif var.Temperature < 0:
+            varme.start(0)
+            varme.ChangeDutyCycle(70)
+        else:
+            cooler.stop()
+            peltier.stop()
 
 @app.route('/option2') # 3-5 grader (mælk)
 def option2():
-    print ("Setting prefered temperature to 3-5 degrees")   
-    return ("nothing")
+    var.mode = 2
+    while var.mode == 2:
+        if var.Temperature > 5:
+            cooler.start(0)
+            peltier.start(0)
+            cooler.ChangeDutyCycle(100)
+            peltier.ChangeDutyCycle(70)
+        elif var.Temperature < 3:
+            varme.start(0)
+            varme.ChangeDutyCycle(70)
+        else:
+            cooler.stop()
+            peltier.stop()
+            varme.stop()
 
 @app.route('/option3') # 6-8 grader (grønt)
 def option3():
-    print ("Setting prefered temperature to 6-8 degrees")
-    return ("nothing")
+    var.mode = 3
+    while var.mode == 3:
+        if var.Temperature > 8:
+            cooler.start(0)
+            peltier.start(0)
+            cooler.ChangeDutyCycle(100)
+            peltier.ChangeDutyCycle(70)
+        elif var.Temperature < 6:
+            varme.start(0)
+            varme.ChangeDutyCycle(70)
+        else:
+            cooler.stop()
+            peltier.stop()
+            varme.stop()
 
 
 
@@ -43,11 +94,10 @@ def option3():
 def data():
     dataTime, dataTemp = getLastData()
 
-
-    Temperature = dataTemp
+    var.Temperature = dataTemp
     Time = dataTime
 
-    data = [time() * 1000, Temperature]
+    data = [Time, var.Temperature]
 
     response = make_response(json.dumps(data))
 
